3a07619d9cf50e418d619a0f01815bf0f480a839
Jacky Alciné <me@jalcine.me>
2014-03-24 15:24:03 -0400

    Updated my site, WAY cool.

--- Gemfile
+++ Gemfile
@@ -4,11 +4,11 @@ ruby '2.0.0'
 gem 'rb-fsevent'
 gem 'libnotify'
 
-gem 'haml', '4.0.3'
 gem 'sass'
 gem 'compass'
 gem 'redcarpet', '~> 2.3.0'
 gem 'yui-compressor'
+gem 'haml'
 
 gem 'jekyll', '1.4.3'
 gem 'jekyll-haml', '~> 0.1.1'
--- src/js/page.js
+++ src/js/page.js
@@ -3,18 +3,10 @@
 
   var WebFont = {
     google: {
-      families: ['Lato']
+      families: ['Lato', 'Cabin']
     }
   };
 
-  var jalcine = {
-    stickyHeader : {
-      header: null,
-      init : function() {
-      }
-    },
-    init : function() {
-      // TODO Add the listener for the sticky header.
-    }
+  var jalcine = {};
   };
 })();
