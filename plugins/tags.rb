8605d0c2b77a7fdeebe80a697ab4b2e63719051f
Jacky Alciné <me@jalcine.me>
2015-02-04 23:21:46 -0500

    Update logic for loading tags.

--- plugins/tags.rb
+++ plugins/tags.rb
@@ -1,12 +1,15 @@
 require_relative '../lib/timeline'
 require_relative '../lib/liquid/tags'
 
-TAGS_TO_ADD = {
-  'tl_event'        => Timeline::EventBlock,
-  'twitter'         => TwitterJekyll::TwitterTag,
-  'twitternocache'  => TwitterJekyll::TwitterTagNoCache,
+TAGS_TO_ADD =
+{
+  'tl_event'      => Timeline::EventBlock,
+  'tweet'         => Jekyll::TweetTag,
+  'tweetnocache'  => Jekyll::TweetTagNoCache,
 }
 
-TAGS_TO_ADD.each do | tag_name, tag_class |
-  Liquid::Template.register_tag(tag_name, tag_class)
+def apply_tag(name, klass)
+  Liquid::Template.register_tag(name, klass)
 end
+
+TAGS_TO_ADD.each apply_tag
