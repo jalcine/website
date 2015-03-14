86410ab9e70991bfcf06acd05660583b65b0c1f9
Jacky Alciné <me@jalcine.me>
2015-03-14 07:51:32 -0400

    touch up and fix height in blink/webkit.

--- lib/mina/rsync.rb
+++ lib/mina/rsync.rb
@@ -1,20 +1,25 @@
 namespace :rsync do
   task :copy do
     src_dir = settings[:rsync_source]
-    dest_dir = "#{settings[:user]}@#{settings[:domain]}:#{deploy_to}/tmp/rsync"
 
-    rsync_command = ['rsync']
-    rsync_command += settings[:rsync_options]
-    rsync_command += [ src_dir ]
-    rsync_command += [ dest_dir ]
-    rsync_command = rsync_command.join(' ')
-    puts "-> Invoking #{rsync_command}.."
-    Kernel.system rsync_command
+    if Dir.glob(src_dir).length != 0
+      dest_dir = "#{settings[:user]}@#{settings[:domain]}:#{deploy_to}/tmp/rsync"
 
-    print_msg "Moving from #{deploy_to} to build directory..."
-    queue %{
-      mv -v #{deploy_to}/tmp/rsync/* .
-    }
+      rsync_command = ['rsync']
+      rsync_command += settings[:rsync_options]
+      rsync_command += [ src_dir ]
+      rsync_command += [ dest_dir ]
+      rsync_command = rsync_command.join(' ')
+      puts "-> Invoking #{rsync_command}.."
+      Kernel.system rsync_command
+
+      print_msg "Moving from #{deploy_to} to build directory..."
+      queue %{
+        mv -v #{deploy_to}/tmp/rsync/* .
+      }
+    else
+      print_msg "-> Can't copy, src_dir is empty."
+    end
   end
 
   task :setup do
