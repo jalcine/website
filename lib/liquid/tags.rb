e40f1817795fe5cac3cc1950de40ef6fbf6ca7fc
Jacky Alcine <yo@jacky.wtf>
2015-06-19 18:27:43 -0400

    add new posts.

--- lib/liquid/tags.rb
+++ lib/liquid/tags.rb
@@ -1,3 +1,2 @@
 require_relative 'tags/youtube'
-require_relative 'tags/twitter'
 require_relative 'tags/clean_the_web'
--- plugins/bundler.rb
+++ plugins/bundler.rb
@@ -1,5 +1,7 @@
 require 'rubygems'
 require 'bundler/setup'
+require 'dotenv'
 
 Bundler.require
+Dotenv.load
 Bundler.require(:jekyll)
