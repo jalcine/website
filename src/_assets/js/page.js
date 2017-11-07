bc3777c3d5b056bc492a466b420b8761e103a963
Jacky Alciné <yo@jacky.wtf>
2017-11-07 10:03:05 -0800

    Add some more projects.

--- src/_assets/js/page.js
+++ src/_assets/js/page.js
@@ -5,7 +5,7 @@ var WebFontConfig = {
   google: {
     families: [
       'Open+Sans:400,700,italic:latin', // Default page font
-      'Slabo:400,700:latin', // Default page font
+      'Merriweather:400,700:latin', // Default page font
       'Megrim:400:latin', // Default page font
       'Inconsolata:100,300,500,700,italic:latin', // Default page font
     ]
