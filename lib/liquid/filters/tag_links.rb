c8c7e9119f4af2ccee0e1f31d9f20dc8894c3e88
Jacky Alciné <yo@jacky.wtf>
2018-09-10 15:06:54 -0700

    fix it

--- lib/liquid/filters/tag_links.rb
+++ lib/liquid/filters/tag_links.rb
@@ -1,6 +1,17 @@
+# frozen_string_literal: true
+
 module TagLinksFilter
   def linkify_tags(list)
-    list.map! { |v| "<a class='p-category' target='_blank' href='/weblog/tag/#{v}/' title='#{v}'>#{v}</a>" }
+    list.map! do |tag_name|
+      <<LINK
+  <a class="p-category"
+    target="_blank"
+    href="/weblog/tag/#{tag_name}/"
+    title="#{tag_name}">
+    #{tag_name}
+  </a>
+LINK
+    end
     list.join ', '
   end
 end
